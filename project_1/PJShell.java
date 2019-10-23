5c8f52c5628cefaeda2e51a5d347ecbd48ebc488
Blake Williams <duckbill43@gmail.com>
2019-10-23 12:32:46 -0400

    high level outline started

--- project_1/PJShell.java
+++ project_1/PJShell.java
@@ -4,5 +4,23 @@ public class PJShell{
 		for (int i=0;i<args.length;i++){
 			System.out.println(args[i]);
 		}
+
+		int current_flag = 0;
+
+		if (args.length == 0){
+			// print bash prompt
+		}
+
+		if (args[current_flag++] == "ls"){
+			// do some listing stuff here
+		} else if (args[current_flag++] == "rm"){
+			// do some remove shit here
+		} else if (args[current_flag++] == "cp"){
+			// do some copy shit here
+		} else if (args[current_flag++] == "mkdir"){
+			// make a directory
+		} else if (args[current_flag++] == "rmdir"){
+			// remove a directory
+		}
 	}
 }
