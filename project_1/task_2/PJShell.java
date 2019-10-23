e22c587b1484152ba91bebb05ed95996c463568e
Blake Williams <duckbill43@gmail.com>
2019-10-23 13:42:17 -0400

    separating methods and updating string comparisons

--- project_1/task_2/PJShell.java
+++ project_1/task_2/PJShell.java
@@ -1,10 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 
 public class PJShell{
 
+	public static void ls_method(String args[]){
+		String path = ".";
+		if (args.length > 0){
+			path = args[0];
+		}
+		// do list printing here
+		System.out.println("listing current children");
+	}
+
 	public static void ShellCommand(String args[]){
-		int current_flag = 0;
+		int current_word = 0;
 
 		// for (int i=0;i<args.length;i++){
 		// 	System.out.println(args[i]);
@@ -13,15 +22,17 @@ public class PJShell{
 
 		if (args.length == 0){
 			// do nothing, go to the bottom and print prompt
-		} else if (args[current_flag] == "ls"){
-			// do some listing stuff here
-		} else if (args[current_flag] == "rm"){
+		} else if (args[current_word].equals("ls")){
+			current_word++;
+			ls_method(Arrays.copyOfRange(args, current_word, args.length));
+			
+		} else if (args[current_word].equals("rm")){
 			// do some remove shit here
-		} else if (args[current_flag] == "cp"){
+		} else if (args[current_word].equals("cp")){
 			// do some copy shit here
-		} else if (args[current_flag] == "mkdir"){
+		} else if (args[current_word].equals("mkdir")){
 			// make a directory
-		} else if (args[current_flag] == "rmdir"){
+		} else if (args[current_word].equals("rmdir")){
 			// remove a directory
 		}
 
