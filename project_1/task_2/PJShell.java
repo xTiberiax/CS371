335c98e9da62a31c7dcc780009776bbc7d5acc68
Blake Williams <duckbill43@gmail.com>
2019-10-23 14:32:35 -0400

    filling out method prototypes

--- project_1/task_2/PJShell.java
+++ project_1/task_2/PJShell.java
@@ -3,14 +3,47 @@ import java.util.*;
 
 public class PJShell{
 
-	public static void ls_method(String args[]){
+	public static int ls_method(String args[], int current_word){
 		String path = ".";
-		if (args.length > 0){
-			path = args[0];
+		if (current_word < args.length){
+			path = args[current_word++];
 		}
-		// do list printing here
-		System.out.println("listing current children");
+		// do list printing and exceptions here
+		System.out.println("listing current children in directory: " + path);
+		return current_word;
 	}
+
+	public static int rm_method(String args[], int current_word){
+		String rm_path = "";
+		while (current_word < args.length){
+			if (args[current_word].equals(";")){
+				break;
+			} else {
+				rm_path = args[current_word++];
+				// do removal
+				System.out.println("removing file: " + rm_path);
+			}
+		}
+		if (rm_path.equals("")){
+			// throw no path exception
+			System.out.println(" Error: You must provide a file path to remove");
+		}
+		return current_word;
+	}
+
+	public static int cp_method(String args[], int current_word){
+		return 0;
+	}
+
+	public static int mkdir_method(String args[], int current_word){
+		return 0;
+	}
+
+	public static int rmdir_method(String args[], int current_word){
+		return 0;
+	}
+
+
 
 	public static void ShellCommand(String args[]){
 		int current_word = 0;
@@ -19,23 +52,30 @@ public class PJShell{
 		// 	System.out.println(args[i]);
 		// }
 
+		while(current_word < args.length){
 
-		if (args.length == 0){
-			// do nothing, go to the bottom and print prompt
-		} else if (args[current_word].equals("ls")){
-			current_word++;
-			ls_method(Arrays.copyOfRange(args, current_word, args.length));
-			
-		} else if (args[current_word].equals("rm")){
-			// do some remove shit here
-		} else if (args[current_word].equals("cp")){
-			// do some copy shit here
-		} else if (args[current_word].equals("mkdir")){
-			// make a directory
-		} else if (args[current_word].equals("rmdir")){
-			// remove a directory
-		}
+			if (args[current_word].equals("ls")){
+				current_word = ls_method(args, ++current_word);
+				
+			} else if (args[current_word].equals("rm")){
+				current_word = rm_method(args, ++current_word);
 
+			} else if (args[current_word].equals("cp")){
+				current_word = cp_method(args, ++current_word);
+
+			} else if (args[current_word].equals("mkdir")){
+				current_word = mkdir_method(args, ++current_word);
+				
+			} else if (args[current_word].equals("rmdir")){
+				current_word = rmdir_method(args, ++current_word);
+				
+			} else if (args[current_word].equals(";")){
+				// go through another execution cycle
+			} else {
+				// dont recognize a command. For now, just ignore it
+				current_word++;
+			}
+		} 
 
 		System.out.print("pjshell> ");
 	}
@@ -48,6 +88,10 @@ public class PJShell{
 			ShellCommand(shell_args);
 			shell_args = keyboard.nextLine().split(" ");
 		}
+
+		System.out.println(
+			"~~~~~~~~~~~~~~~~~\nThank you for using pjshell\n~~~~~~~~~~~~~~~~~"
+			);
 	}
 
 }
