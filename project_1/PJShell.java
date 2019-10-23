f1eef94e7c2298c3315868c3e2daa58154a0a3d4
Blake Williams <duckbill43@gmail.com>
2019-10-23 12:24:05 -0400

    proof of agrument acception

--- project_1/PJShell.java
+++ project_1/PJShell.java
@@ -1,6 +1,8 @@
 
 public class PJShell{
 	public static void main(String args[]){
-		System.out.println("hello world");
+		for (int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
 	}
 }
